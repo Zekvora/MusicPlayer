1c63b421a70ea06be38c11c71efb4be1c44127f6
Zekvora <dencik1425@hotmail.com>
2026-01-17 13:31:12 +0200

    Update CrashHandler.java

--- app/src/main/java/com/zekvora/musicplayer/CrashHandler.java
+++ app/src/main/java/com/zekvora/musicplayer/CrashHandler.java
@@ -1,60 +1,23 @@
 package com.zekvora.musicplayer;
 
 import android.app.Application;
-import android.content.DialogInterface;
-import android.os.Process;
-import androidx.appcompat.app.AlertDialog;
+import android.app.AlertDialog;
+import android.os.Handler;
+import android.os.Looper;
 
 public class CrashHandler implements Thread.UncaughtExceptionHandler {
 
-    private final Application app;
-    private final Thread.UncaughtExceptionHandler defaultHandler;
-
-    public CrashHandler(Application app) {
-        this.app = app;
-        this.defaultHandler = Thread.getDefaultUncaughtExceptionHandler();
-    }
-
     public static void install(Application app) {
-        Thread.setDefaultUncaughtExceptionHandler(new CrashHandler(app));
-    }
-
-    @Override
-    public void uncaughtException(Thread thread, Throwable throwable) {
-        String msg = getStackTraceString(throwable);
-
-        // Показываем диалог в UI-потоке
-        new android.os.Handler(app.getMainLooper()).post(() -> {
-            new AlertDialog.Builder(app)
-                    .setTitle("ОШИБКА")
-                    .setMessage(msg)
-                    .setCancelable(false)
-                    .setPositiveButton("ВЫЙТИ", (d, w) -> {
-                        d.dismiss();
-                        Process.killProcess(Process.myPid());
-                        System.exit(1);
-                    })
-                    .show();
+        Thread.setDefaultUncaughtExceptionHandler((t, e) -> {
+            String msg = e.getClass().getSimpleName() + ": " + e.getMessage();
+            new Handler(Looper.getMainLooper()).post(() -> {
+                new AlertDialog.Builder(app)
+                        .setTitle("КРАШ")
+                        .setMessage(msg)
+                        .setCancelable(false)
+                        .setPositiveButton("OK", null)
+                        .show();
+            });
         });
-
-        // Через 5 секунды убиваем процесс (чтобы диалог не висел вечно)
-        new android.os.Handler(app.getMainLooper()).postDelayed(() -> {
-            Process.killProcess(Process.myPid());
-            System.exit(1);
-        }, 5000);
-
-        // Запускаем старый обработчик на всякий случай
-        if (defaultHandler != null) {
-            defaultHandler.uncaughtException(thread, throwable);
-        }
-    }
-
-    private static String getStackTraceString(Throwable e) {
-        StringBuilder sb = new StringBuilder();
-        sb.append(e.getClass().getSimpleName()).append(": ").append(e.getMessage()).append("\n\n");
-        for (StackTraceElement el : e.getStackTrace()) {
-            sb.append("  at ").append(el).append('\n');
-        }
-        return sb.toString();
     }
 }
