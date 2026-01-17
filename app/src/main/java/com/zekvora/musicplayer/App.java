ec6b6a9473c009b0290490ca28eb049e4e68f86f
Zekvora <dencik1425@hotmail.com>
2026-01-17 13:12:02 +0200

    Add App class for music player application

---
+++ app/src/main/java/com/zekvora/musicplayer/App.java
@@ -0,0 +1,10 @@
+package com.zekvora.musicplayer;
+import android.app.Application;
+
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        CrashHandler.install(this);
+    }
+}
