8018dcadea6cda439cb1dcb830ccde563ae4e6d5
Zekvora <dencik1425@hotmail.com>
2026-01-17 11:37:21 +0200

    Fix: add package/imports and placeholder PlayerActivity; set manifest package

--- app/src/mine/java/com/example/musicplayer/PlayerActivity.kt
+++ app/src/mine/java/com/example/musicplayer/PlayerActivity.kt
@@ -3,10 +3,10 @@ package com.example.musicplayer
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class PlayerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Simple placeholder layout; reuse activity_main for now
         setContentView(R.layout.activity_main)
-        // TODO: реализуй список песен
     }
 }
