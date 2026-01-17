99f6473834222b374335d1d679dc21207ab69713
Zekvora <dencik1425@hotmail.com>
2026-01-17 13:31:02 +0200

    Update App.java

--- app/src/main/java/com/zekvora/musicplayer/App.java
+++ app/src/main/java/com/zekvora/musicplayer/App.java
@@ -1,10 +1,12 @@
 package com.zekvora.musicplayer;
+
 import android.app.Application;
+import android.widget.Toast;
 
 public class App extends Application {
-    @Override
-    public void onCreate() {
+    @Override public void onCreate() {
         super.onCreate();
+        Toast.makeText(this, "App запущена", Toast.LENGTH_LONG).show();
         CrashHandler.install(this);
     }
 }
